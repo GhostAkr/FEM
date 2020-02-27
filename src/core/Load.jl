e7377900c0d1d0c3add8e261bd822d8dcac5f0bb
Alexander Antoshkin <ighost99@gmail.com>
2020-02-27 23:59:44 +0300

    [*] Changed source files structure. [+] Added process properties.

--- src/core/Load.jl
+++ src/core/Load.jl
@@ -1,13 +1,5 @@
-# Loads and boundary conditions description
-
-module Load
-
-export bc
-
 @enum bc begin
     fixedX  # Dislpacement fixed by X
     fixedY  # Dislpacement fixed by Y
     fixedXY  # Dislpacement fixed by X and Y
 end
-
-end  # Load
