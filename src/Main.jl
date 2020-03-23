be68b525e75d41dd46ad9baf867cdd1996517a29
Alexander Antoshkin <ighost99@gmail.com>
2020-03-23 18:11:47 +0300

    Technical commit

--- src/Main.jl
+++ src/Main.jl
@@ -1,5 +1,8 @@
 # To provide correct modules loading it's important to execeute Set_Path.jl.
 
 using CoreFEM
+using HCubature
 
 CoreFEM.fem2D()
+
+f(x) = x
--- src/core/StiffnessMatrix.jl
+++ src/core/StiffnessMatrix.jl
@@ -1,11 +1,19 @@
 export stiffnessMatrix
 
 using Quad4Pts
+using HCubature
+
+function F(r, s, xCoords::Array{Float64}, yCoords::Array{Float64}, elasticityMatrix::AbstractArray)  # F = B^T * C * B * det(J)
+    B = Quad4Pts.gradMatr(r, s, xCoords, yCoords)  # Gradient matrix
+    BTransp = transpose(B)
+    J = Quad4Pts.jacGlobToLoc(r, s, xCoords, yCoords)  # Jacobi's matrix
+    return B * elasticityMatrix * BTransp * det(J)
+end
 
 function stiffnessMatrix(elasticityMatrix::AbstractArray, parameters::processPars, elementNum::Int)
     xCoords = [parameters.mesh.nodes[parameters.mesh.elements[elementNum][i]][1] for i in 1:4]
     yCoords = [parameters.mesh.nodes[parameters.mesh.elements[elementNum][i]][2] for i in 1:4]
+    thickness = 1
+    
 end
-
-jacGlobToLoc() = []
 
