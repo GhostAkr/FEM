0a9694106a9bee3ae1f19fbe1048a18d319cecc9
Alexander Antoshkin <ighost99@gmail.com>
2020-06-10 20:45:12 +0300

    [+] Added example mesh. [+] Added import of Salome mesh in .dat format.

---
+++ src/mesh/MeshVars.jl
@@ -0,0 +1,11 @@
+export meshType
+
+"""
+    meshType
+
+Enum containing different mesh types:
+- 2DQuad4Pts.
+"""
+@enum meshType begin
+    Quad4Pts2D
+end
--- src/mesh/Mesh_T.jl
+++ src/mesh/Mesh_T.jl
@@ -1,4 +1,6 @@
-export Mesh2D_T, generateTestMesh2D, printNodesMesh2D, printElementsMesh2D
+include("MeshVars.jl")
+
+export Mesh2D_T, generateTestMesh2D, printNodesMesh2D, printElementsMesh2D, readMeshFromSalomeDAT
 
 """
     Mesh2D_T
@@ -68,3 +70,56 @@ function printElementsMesh2D(mesh::Mesh2D_T)
     end
     print("\n")
 end  # printElementsMesh2D
+
+"""
+    readMeshFromSalomeDAT(pathToFile::String)
+
+Read mesh from .dat file generated via Salome platform.
+
+# Arguments
+- `pathToFile::String`: Path to given file;
+- `type::meshType`: Type of given mesh.
+"""
+function readMeshFromSalomeDAT(pathToFile::String, type::meshType)
+    mesh = Mesh2D_T(Vector{Tuple{Vararg{Float64}}}(undef, 0), Vector{Tuple{Vararg{Int}}}(undef, 0))
+    open(pathToFile, "r") do file
+        fileContents = split(read(file, String))  # Read once to provide more efficiency on big meshes
+        nOfNodes = parse(Int, fileContents[1])
+        nOfEntities = parse(Int, fileContents[2])
+        nodes = Vector{Tuple{Vararg{Float64}}}(undef, nOfNodes)
+        for i in 1:nOfNodes
+            nodeIndex = parse(Int, fileContents[(i - 1) * 4 + 3])
+            nodeX = parse(Float64, fileContents[(i - 1) * 4 + 4])
+            nodeY = parse(Float64, fileContents[(i - 1) * 4 + 5])
+            nodeZ = parse(Float64, fileContents[(i - 1) * 4 + 6])
+            nodes[nodeIndex] = (nodeX, nodeY, nodeZ)
+        end
+        if type == Quad4Pts2D
+            salomeTypeId = "204"
+            nodesPerElement = 4
+        else
+            println("Given mesh type is not supported")
+        end
+        nOfElements = size(findall(isequal(salomeTypeId), fileContents))[1]
+        elements = Vector{Tuple{Vararg{Float64}}}()
+        elemPos = findfirst(isequal(salomeTypeId), fileContents) - 1
+        while findnext(isequal(salomeTypeId), fileContents, elemPos) !== nothing
+            elemNodes = Tuple{Vararg{Float64}}
+            nextIdPos = findnext(isequal(salomeTypeId), fileContents, elemPos + 2)
+            if nextIdPos !== nothing
+                lastNode = nextIdPos - 2
+            else
+                lastNode = size(fileContents)[1]
+            end
+            nodesArray = Vector{Float64}()
+            for i in (elemPos + 2):lastNode
+                push!(nodesArray, parse(Float64, fileContents[i]))
+            end
+            elemNodes = Tuple(nodesArray)
+            push!(elements, elemNodes)
+            elemPos = lastNode + 1
+        end
+        mesh = Mesh2D_T(nodes, elements)
+        return mesh
+    end
+end  # readMeshFromSalomeDAT
