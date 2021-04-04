a4998b5861f2c92a6cf59386c9c82deaf7743e1c
GhostAkr <ighost99@gmail.com>
2021-04-04 18:21:50 +0300

    [-] Removed not working tests from Main.jl.

--- src/Main.jl
+++ src/Main.jl
@@ -4,8 +4,6 @@ using CoreFEM
 using ElementTypes
 
 # Example tests
-CoreFEM.fem2D("examples/QuarterPlate8N/QuadrPlate8N_MeshNew.dat", "examples/QuarterPlate8N/QuadrPlate8N_DataNew", Quad8TypeID)
-CoreFEM.fem2D("examples/QuarterPlate/Quarter_Mesh.dat", "examples/QuarterPlate/Quarter_Data.json", Quad4TypeID)
 CoreFEM.fem2D("examples/SmallPlate/PlateMeshSmall.med", "examples/SmallPlate/SmallPlate_Data.json", Quad4TypeID)
 CoreFEM.fem2D("examples/Beam/BeamMesh.med", "examples/Beam/BeamData.json", Quad4TypeID)
 
