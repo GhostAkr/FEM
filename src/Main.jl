1457cebf15eabf450ee45b53de0b98db5ab011d8
Alexander Antoshkin <ighost99@gmail.com>
2021-06-21 21:02:14 +0300

    [+] Updated examples. - Added Beam2DBindAnsys example. - Updated Beam3DBindAnsys example.

--- src/Main.jl
+++ src/Main.jl
@@ -7,6 +7,7 @@ using ElementTypes
 CoreFEM.fem2D("examples/SmallPlate/PlateMeshSmall.med", "examples/SmallPlate/SmallPlate_Data.json", Quad4TypeID)
 CoreFEM.fem2D("examples/Beam/BeamMesh.med", "examples/Beam/BeamData.json", Quad4TypeID)
 CoreFEM.fem2D("examples/SimpleBeam2D/SimpleBeam2D.med", "examples/SimpleBeam2D/SimpleBeam2D.json", Quad4TypeID)
+CoreFEM.fem2D("examples/Beam2DBindAnsys/Beam2DAnsys.med", "examples/Beam2DBindAnsys/Beam2DBindAnsys.json", Quad4TypeID)
 
 CoreFEM.fem3D("examples/Beam3D/Beam3D.med", "examples/Beam3D/Beam3D.json", Iso8Pts3DTypeID)
 CoreFEM.fem3D("examples/Beam3DBindSimple/Beam3DBindSimple.med", "examples/Beam3DBindSimple/Beam3DBindSimple.json", Iso8Pts3DTypeID)
